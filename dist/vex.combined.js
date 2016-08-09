da0e3c567d23490dc9001bd75fd0801525acfb30
Brendan Batliner <brendan.batliner@gmail.com>
2016-08-09 14:58:41 -0500

    Build beta

--- dist/vex.combined.js
+++ dist/vex.combined.js
@@ -1098,17 +1098,6 @@ var buttonsToDOM = function buttonsToDOM (buttons) {
   return domButtons
 }
 
-// Use the DOM's HTML parsing to escape any dangerous strings
-var escapeHtml = function escapeHtml (str) {
-  if (typeof str !== 'undefined') {
-    var div = document.createElement('div')
-    div.appendChild(document.createTextNode(str))
-    return div.innerHTML
-  } else {
-    return ''
-  }
-}
-
 var plugin = function plugin (vex) {
   // Define the API first
   var dialog = {
@@ -1124,7 +1113,7 @@ var plugin = function plugin (vex) {
       if (options.unsafeMessage && !options.message) {
         options.message = options.unsafeMessage
       } else if (options.message) {
-        options.message = escapeHtml(options.message)
+        options.message = vex._escapeHtml(options.message)
       }
 
       // Build the form from the options
@@ -1188,7 +1177,7 @@ var plugin = function plugin (vex) {
 
     // Confirm
     confirm: function (options) {
-      if (typeof options === 'string') {
+      if (typeof options !== 'object' || typeof options.callback !== 'function') {
         throw new Error('dialog.confirm(options) requires options.callback.')
       }
       options = Object.assign({}, this.defaultOptions, this.defaultConfirmOptions, options)
@@ -1197,13 +1186,13 @@ var plugin = function plugin (vex) {
 
     // Prompt
     prompt: function (options) {
-      if (typeof options === 'string') {
+      if (typeof options !== 'object' || typeof options.callback !== 'function') {
         throw new Error('dialog.prompt(options) requires options.callback.')
       }
       var defaults = Object.assign({}, this.defaultOptions, this.defaultPromptOptions)
       var dynamicDefaults = {
-        unsafeMessage: '<label for="vex">' + (escapeHtml(options.label) || defaults.label) + '</label>',
-        input: '<input name="vex" type="text" class="vex-dialog-prompt-input" placeholder="' + (options.placeholder || defaults.placeholder) + '" value="' + (options.value || defaults.value) + '" />'
+        unsafeMessage: '<label for="vex">' + vex._escapeHtml(options.label || defaults.label) + '</label>',
+        input: '<input name="vex" type="text" class="vex-dialog-prompt-input" placeholder="' + vex._escapeHtml(options.placeholder || defaults.placeholder) + '" value="' + vex._escapeHtml(options.value || defaults.value) + '" />'
       }
       options = Object.assign(defaults, dynamicDefaults, options)
       // Pluck the value of the "vex" input field as the return value for prompt's callback
@@ -1260,7 +1249,6 @@ var plugin = function plugin (vex) {
   }
 
   dialog.defaultAlertOptions = {
-    message: 'Alert',
     buttons: [
       dialog.buttons.YES
     ]
@@ -1272,9 +1260,7 @@ var plugin = function plugin (vex) {
     value: ''
   }
 
-  dialog.defaultConfirmOptions = {
-    message: 'Confirm'
-  }
+  dialog.defaultConfirmOptions = {}
 
   return dialog
 }
